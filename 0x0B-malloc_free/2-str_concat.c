d82f096f4ac2c72a3d4ef4f0e4e83bb6cd4bbe23
0xayman <aymantarig17@gmail.com>
2023-09-04 07:51:03 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,36 +12,36 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *result;
-    int i = 0, j = 0;
-    int len1 = 0, len2 = 0;
+	char *result;
+	int i = 0, j = 0;
+	int len1 = 0, len2 = 0;
 
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
-    len1 = strlen(s1);
-    len2 = strlen(s2);
-    result = malloc(sizeof(char) * (len1 + len2 + 1));
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	 result = malloc(sizeof(char) * (len1 + len2 + 1));
 
-    if (result == NULL)
-    {
-        return (NULL);
-    }
+	if (result == NULL)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; i < len1; i++)
-    {
-        *(result + i) = *(s1 + i);
-    }
-    for (j = len1; j < (len1 + len2); j++)
-    {
-        *(result + j) = *(s2 + j - len1);
-    }
-    return (result);
+	for (i = 0; i < len1; i++)
+	{
+		*(result + i) = *(s1 + i);
+	}
+	for (j = len1; j < (len1 + len2); j++)
+	{
+		*(result + j) = *(s2 + j - len1);
+	}
+	return (result);
 }
 
