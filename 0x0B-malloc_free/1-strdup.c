1eb2f93801bc0a2d6c9ffecf93fc8fac2143dddf
0xayman <aymantarig17@gmail.com>
2023-09-04 07:10:13 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,37 +1,38 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * argstostr - concatenate all args
- * @ac: argc
- * @av: arguments
- *
- * Return: array
+ * _strdup - duplicate a string
+ * @str: string to copy
+ * Return: duplicated string
  */
 
 char *_strdup(char *str)
 {
+
+	int i;
+	int c = 0;
+	char *t;
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	int count = 0;
-	while (str[count] != '\0')
+	while (str[c] != '\0')
 	{
-		count++;
+		c++;
 	}
 
-	char *t;
-	t = malloc(size(char) * c + 1);
-	
+	t = malloc(sizeof(char) * c + 1);
+
 	if (t == NULL)
 	{
 		return (NULL);
 	}
-	for (int i = 0; i < count; i++)
+	for (i = 0; i < c; i++)
 	{
 		t[i] = str[i];
 	}
-	return t;
+	return (t);
 }
