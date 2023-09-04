9023f5da7f14b06f9a2e4dcaa294d591b46cfe3e
0xayman <aymantarig17@gmail.com>
2023-09-04 07:41:59 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
     char *result;
     int i = 0, j = 0;
     int len1 = 0, len2 = 0;
-    
+
     if (s1 == NULL)
     {
         s1 = "";
@@ -24,16 +24,16 @@ char *str_concat(char *s1, char *s2)
     {
         s2 = "";
     }
-    
+
     len1 = strlen(s1);
     len2 = strlen(s2);
     result = malloc(sizeof(char) * (len1 + len2 + 1));
-    
+
     if (result == NULL)
     {
         return (NULL);
     }
-    
+
     for (i = 0; i < len1; i++)
     {
         *(result + i) = *(s1 + i);
@@ -45,22 +45,3 @@ char *str_concat(char *s1, char *s2)
     return (result);
 }
 
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s;
-
-    s = str_concat("Betty ", "Holberton");
-    if (s == NULL)
-    {
-        printf("failed\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
-}
