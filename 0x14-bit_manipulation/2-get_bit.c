da948ebeaa72f8da65d478c5817cc07b78f80e20
0xayman <aymantarig17@gmail.com>
2023-09-28 14:47:42 +0200

    fix betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,15 +7,15 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    unsigned long int mask = 1;
+	unsigned long int mask = 1;
 
-    if (index > sizeof(unsigned long int) * 8)
-        return (-1);
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
 
-    mask <<= index;
+	mask <<= index;
 
-    if ((n & mask) == 0)
-        return (0);
-    else
-        return (1);
+	if ((n & mask) == 0)
+		return (0);
+	else
+		return (1);
 }
