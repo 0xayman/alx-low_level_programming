957eaa24d76e83b061f9a63a5293497b44777e70
0xayman <aymantarig17@gmail.com>
2023-10-02 14:32:34 +0200

    fix code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,53 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ *         0 if the file can not be opened or read
+ *
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i, y;
-	char *buf;
+	char *buffer;
+	int file_discriptor;
+	ssize_t read_bytes;
+	ssize_t write_bytes;
 
 	if (!filename)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+
+	file_discriptor = open(filename, O_RDONLY);
+
+	if (file_discriptor < 0)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (!buffer)
 		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
+
+	read_bytes = read(file_discriptor, buffer, letters);
+
+	if (read_bytes < 0)
 	{
-		free(buf);
+		free(buffer);
 		return (0);
 	}
-	buf[i] = '\0';
-	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
+
+	buffer[read_bytes] = '\0';
+	close(file_discriptor);
+
+	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+
+	if (write_bytes < 0)
 	{
-		free(buf);
+		free(buffer);
 		return (0);
 	}
-	free(buf);
-	return (y);
+
+	free(buffer);
+
+	return (write_bytes);
 }
