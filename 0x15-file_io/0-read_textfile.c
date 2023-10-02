ec66efda2a6e37893502359c7daf8ad408e945e8
0xayman <aymantarig17@gmail.com>
2023-10-02 18:01:24 +0200

    fix description

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,6 @@
  * @filename: name of the file
  * @letters: number of letters it should read and print
  * Return: actual number of letters it could read and print, or 0 if fails
- *
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
